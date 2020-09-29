fbf1e4b16d3a066b9d9b4478354cc8f564c5a577
Tyler Kummer <tylerkummer@protonmail.com>
2020-09-29 11:21:44 -0500

    Updates

---
+++ precache-manifest.0b74a6531a632dee431148dba98d92bf.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d086b39e6a0a9ee55285fa928491003c",
+    "url": "/pokemon-api/index.html"
+  },
+  {
+    "revision": "afb8567e44ae1b9cb846",
+    "url": "/pokemon-api/static/css/main.2530dcda.chunk.css"
+  },
+  {
+    "revision": "0bdeed6d5f6b6ceba5b7",
+    "url": "/pokemon-api/static/js/2.903aa597.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/pokemon-api/static/js/2.903aa597.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "afb8567e44ae1b9cb846",
+    "url": "/pokemon-api/static/js/main.1b8cc067.chunk.js"
+  },
+  {
+    "revision": "bff03650fd0e48bac186",
+    "url": "/pokemon-api/static/js/runtime-main.73d973cc.js"
+  }
+]);
